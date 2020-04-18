946dcbea7f0c72a207a8fe7b0bd89f809107b740
Bart Wijnants <bart.w@sparkcentral.com>
2020-04-18 15:46:29 +0200

    Customize tailwind

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # fasterr
-
-## color palette
-
-https://coolors.co/20bf55-0b4f6c-01baef-fbfbff-757575
-
--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from "react";
 import logoFull from "./logo-full.svg";
 
 const App = () => (
-  <div
-    className="h-screen flex items-center justify-center p-4"
-    style={{
-      backgroundColor: "#FBFBFF",
-      color: "#757575",
-    }}
-  >
+  <div className="h-screen flex flex-col items-center justify-center p-4">
     <img src={logoFull} alt="logo" />
+    <h1 className="mt-8 text-4xl text-alpha">ALPHA</h1>
+    <h2 className="mt-4 text-2xl text-beta">BETA</h2>
   </div>
 );
 
